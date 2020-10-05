Fixed variance bug in convert_chain_to_pytorch, sanity check up to layer 1 ok

convert_chain_to_pytorch.py
@@ -65,7 +65,7 @@ def read_stats_mean(file, layer_number, is_tdnnf = True, component_name='', dim=
 	return read_vector(file, '<ComponentName> '+ component_name +' <BatchNormComponent> <Dim> '+dim+' <BlockDim> '+dim+' <Epsilon> 0.001 <TargetRms> 1 <TestMode> F <Count> '+ count +' <StatsMean>  [ ', False)
 
 def read_stats_var(file):
-	return read_vector(file, '<StatsVar>  [ ', False)
+	return read_vector(file, '<StatsVar>  [ ', True)
 
 def read_affine_component(file):
 	params_dict = {}
@@ -76,7 +76,6 @@ def read_affine_component(file):
 def read_batchnorm_component(file, layer_number, is_tdnnf=True, component_name='', dim='1536'):
 	params_dict = {}
 	params_dict['stats_mean'] = read_stats_mean(file, layer_number, is_tdnnf=is_tdnnf, component_name=component_name, dim=dim)
-	line = chain_file.readline()
 	params_dict['stats_var'] = read_stats_var(file)
 	return params_dict
 
@@ -135,19 +134,26 @@ while not finished:
 		#No tdnnfx.dropout yet
 		#No tdnnfx.noop yet
 	
-	'''
+	if '<ComponentName> prefinal-l' in line:
+		components['prefinal-l'] = {}
+		components['prefinal-l']['linear_params'] = read_linear_params(chain_file)
+	
+	#Chain head
 	if '<ComponentName> prefinal-chain.affine' in line:
 		components['prefinal-chain.affine'] = read_affine_component(chain_file)
 
 	if '<ComponentName> prefinal-chain.linear' in line:
 		components['prefinal-chain.linear'] = {}
 		components['prefinal-chain.linear']['linear_params'] = read_linear_params(chain_file)
-	'''
 
-	if '<ComponentName> prefinal-l' in line:
-		components['prefinal-l'] = {}
-		components['prefinal-l']['linear_params'] = read_linear_params(chain_file)
+	if '<ComponentName> prefinal-chain.batchnorm1' in line:
+	 	components['prefinal-chain.batchnorm1'] = read_batchnorm_component(chain_file, 19, component_name='prefinal-chain.batchnorm1')
 
+	if '<ComponentName> prefinal-chain.batchnorm2' in line:
+	 	components['prefinal-chain.batchnorm2'] = read_batchnorm_component(chain_file, 19, component_name='prefinal-chain.batchnorm2', dim='256')
+	
+
+	#Xent head
 	if '<ComponentName> prefinal-xent.affine' in line:
 		components['prefinal-xent.affine'] = read_affine_component(chain_file)
 
@@ -182,6 +188,10 @@ while not finished:
 #print(components['prefinal-xent.linear']['linear_params'].shape)
 #print(components['output-xent.affine']['linear_params'].shape)
 
+#print(components['prefinal-chain.affine']['linear_params'].shape)
+#print(components['prefinal-chain.linear']['linear_params'].shape)
+#print(components['output-xent.affine']['linear_params'].shape)
+
 ftdnn = FTDNN()
 
 state_dict = {}
@@ -193,6 +203,10 @@ state_dict['layer01.kernel.bias'] = torch.from_numpy(components['tdnn1.affine'][
 state_dict['layer01.bn.running_mean'] = torch.from_numpy(components['tdnn1.batchnorm']['stats_mean'])
 state_dict['layer01.bn.running_var'] = torch.from_numpy(components['tdnn1.batchnorm']['stats_var'])
 
+
+print(components['tdnnf2.linear']['linear_params'])
+print(components['tdnnf2.linear']['bias'])
+
 for layer_number in range(2, 18):
 	state_dict['layer'+ str("{:02d}".format(layer_number)) +'.sorth.weight'] = torch.from_numpy(components['tdnnf'+ str(layer_number) +'.linear']['linear_params'])
 	state_dict['layer'+ str("{:02d}".format(layer_number)) +'.affine.weight'] = torch.from_numpy(components['tdnnf'+ str(layer_number) +'.affine']['linear_params'])
@@ -203,17 +217,27 @@ for layer_number in range(2, 18):
 
 state_dict['layer18.weight'] = torch.from_numpy(components['prefinal-l']['linear_params'])
 
-state_dict['layer19.linear1.weight'] = torch.from_numpy(components['prefinal-xent.affine']['linear_params'])
-state_dict['layer19.linear1.bias'] = torch.from_numpy(components['prefinal-xent.affine']['bias'])
-state_dict['layer19.bn1.running_mean'] = torch.from_numpy(components['prefinal-xent.batchnorm1']['stats_mean'])
-state_dict['layer19.bn1.running_var'] = torch.from_numpy(components['prefinal-xent.batchnorm1']['stats_var'])
+# state_dict['layer19.linear1.weight'] = torch.from_numpy(components['prefinal-xent.affine']['linear_params'])
+# state_dict['layer19.linear1.bias'] = torch.from_numpy(components['prefinal-xent.affine']['bias'])
+# state_dict['layer19.bn1.running_mean'] = torch.from_numpy(components['prefinal-xent.batchnorm1']['stats_mean'])
+# state_dict['layer19.bn1.running_var'] = torch.from_numpy(components['prefinal-xent.batchnorm1']['stats_var'])
+# #state_dict['layer19.bn1.num_batches_tracked']
+# state_dict['layer19.linear2.weight'] = torch.from_numpy(components['prefinal-xent.linear']['linear_params'])
+# state_dict['layer19.bn2.running_mean'] = torch.from_numpy(components['prefinal-xent.batchnorm2']['stats_mean'])
+# state_dict['layer19.bn2.running_var'] = torch.from_numpy(components['prefinal-xent.batchnorm2']['stats_var'])
+# #state_dict['layer19.bn2.num_batches_tracked']
+# state_dict['layer19.linear3.weight'] = torch.from_numpy(components['output-xent.affine']['linear_params'])
+# state_dict['layer19.linear3.bias'] = torch.from_numpy(components['output-xent.affine']['bias'])
+
+state_dict['layer19.linear1.weight'] = torch.from_numpy(components['prefinal-chain.affine']['linear_params'])
+state_dict['layer19.linear1.bias'] = torch.from_numpy(components['prefinal-chain.affine']['bias'])
+state_dict['layer19.bn1.running_mean'] = torch.from_numpy(components['prefinal-chain.batchnorm1']['stats_mean'])
+state_dict['layer19.bn1.running_var'] = torch.from_numpy(components['prefinal-chain.batchnorm1']['stats_var'])
 #state_dict['layer19.bn1.num_batches_tracked']
-state_dict['layer19.linear2.weight'] = torch.from_numpy(components['prefinal-xent.linear']['linear_params'])
-state_dict['layer19.bn2.running_mean'] = torch.from_numpy(components['prefinal-xent.batchnorm2']['stats_mean'])
-state_dict['layer19.bn2.running_var'] = torch.from_numpy(components['prefinal-xent.batchnorm2']['stats_var'])
+state_dict['layer19.linear2.weight'] = torch.from_numpy(components['prefinal-chain.linear']['linear_params'])
+state_dict['layer19.bn2.running_mean'] = torch.from_numpy(components['prefinal-chain.batchnorm2']['stats_mean'])
+state_dict['layer19.bn2.running_var'] = torch.from_numpy(components['prefinal-chain.batchnorm2']['stats_var'])
 #state_dict['layer19.bn2.num_batches_tracked']
-state_dict['layer19.linear3.weight'] = torch.from_numpy(components['output-xent.affine']['linear_params'])
-state_dict['layer19.linear3.bias'] = torch.from_numpy(components['output-xent.affine']['bias'])
 
 
 #for param_tensor in ftdnn.state_dict():

pytorch_models.py
@@ -32,6 +32,7 @@ class FTDNNLayer(nn.Module):
             xd = xd[:,:,:-time_offset]
             x = torch.cat([xd, x], axis=2)
         x = self.sorth(x)
+        return x
         if time_offset != 0:
             padding = x[:,:,-1][:,:,None]
             xd = torch.cat([x]+[padding]*time_offset, axis=2)
@@ -73,6 +74,30 @@ class OutputXentLayer(nn.Module):
         softmax = nn.LogSoftmax(dim = 2)
         return softmax(x)
 
+class OutputLayer(nn.Module):
+
+    def __init__(self, linear1_in_dim, linear2_in_dim, out_dim, dropout_p=0.0):
+
+        super(OutputLayer, self).__init__()
+        self.linear1_in_dim = linear1_in_dim
+        self.linear2_in_dim = linear2_in_dim
+        self.out_dim = out_dim
+
+        self.linear1 = nn.Linear(self.linear1_in_dim, self.linear2_in_dim, bias=True) 
+        self.nl = nn.ReLU()
+        self.bn1 = nn.BatchNorm1d(self.linear2_in_dim, affine=False)
+        self.linear2 = nn.Linear(self.linear2_in_dim, self.out_dim, bias=False) 
+        self.bn2 = nn.BatchNorm1d(self.out_dim, affine=False)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.nl(x)
+        x = x.transpose(1,2)
+        x = self.bn1(x).transpose(1,2)
+        x = self.linear2(x)
+        x = x.transpose(1,2)
+        x = self.bn2(x).transpose(1,2)
+        return x
 
 class InputLayer(nn.Module):
 
@@ -81,7 +106,7 @@ class InputLayer(nn.Module):
         input_dim=220,
         output_dim=1536,
         batch_norm=True,
-        dropout_p=0.0):
+        dropout_p=0.005851493):
 
         super(InputLayer, self).__init__()
         self.input_dim = input_dim
@@ -95,7 +120,7 @@ class InputLayer(nn.Module):
         self.nonlinearity = nn.ReLU()
         self.batch_norm = batch_norm
         if batch_norm:
-            self.bn = nn.BatchNorm1d(output_dim, affine=False)
+            self.bn = nn.BatchNorm1d(output_dim, affine=False, eps=0.001)
         self.drop = nn.Dropout(p=self.dropout_p)
 
     def forward(self, x):
@@ -114,11 +139,11 @@ class InputLayer(nn.Module):
         x = self.lda(x)
         x = self.kernel(x)
         x = self.nonlinearity(x)
-        x = self.drop(x)
 
         if self.batch_norm:
             x = x.transpose(1, 2)
             x = self.bn(x).transpose(1,2)
+        x = self.drop(x)
         return x
 
 
@@ -153,7 +178,8 @@ class FTDNN(nn.Module):
         self.layer16 = FTDNNLayer(3072, 160, 320, 1536, 3)
         self.layer17 = FTDNNLayer(3072, 160, 320, 1536, 3)
         self.layer18 = nn.Linear(1536, 256, bias=False) #This is the prefinal-l layer
-        self.layer19 = OutputXentLayer(256, 1536, 256, 6024)
+        #self.layer19 = OutputXentLayer(256, 1536, 256, 6024) #This corresponds to the output-xent layer instead of output
+        self.layer19 = OutputLayer(256, 1536, 256)
 
     def forward(self, x):
 
@@ -161,7 +187,9 @@ class FTDNN(nn.Module):
         Input must be (batch_size, seq_len, in_dim)
         '''
         x = self.layer01(x)
+        return x
         x_2 = self.layer02(x)
+        return x_2
         x, x_2 =sum_outputs_and_feed_to_layer(x,x_2, self.layer03)
         x, x_2 =sum_outputs_and_feed_to_layer(x,x_2, self.layer04)
         x, x_2 =sum_outputs_and_feed_to_layer(x,x_2, self.layer05)
@@ -179,6 +207,5 @@ class FTDNN(nn.Module):
         x, x_2 =sum_outputs_and_feed_to_layer(x,x_2, self.layer17)
         x = self.layer18(x_2)
         x = self.layer19(x)
-
 
         return x