ac809cc5d7e1e02b21ed97cb8042806b2ca82e6a
marcelo sancinetti <msancinetti@dc.uba.ar>
2020-12-10 16:17:21 -0300

    Dumkp loglikes to pickle

--- align_using_pytorch_am.py
+++ align_using_pytorch_am.py
@@ -1,7 +1,7 @@
 from kaldi.asr import MappedLatticeFasterRecognizer
 from kaldi.decoder import LatticeFasterDecoderOptions
 from kaldi.matrix import Matrix
-from kaldi.util.table import SequentialMatrixReader, CompactLatticeWriter
+from kaldi.util.table import SequentialMatrixReader, DoubleMatrixWriter
 from kaldi.nnet3 import NnetSimpleComputationOptions
 from kaldi.alignment import MappedAligner
 from kaldi.fstext import SymbolTable
@@ -27,7 +27,7 @@ text_path = 'epadb/test/text'
 mfccs_rspec = ("ark:epadb/test/data/raw_mfcc_test.1.ark")
 ivectors_rspec = ("ark:epadb/test/data/ivector_online.1.ark")
 
-lat_wspec = "ark:| gzip -c > lat.gz"
+loglikes_wspec = "ark:loglikes.ark"
 
 aligner = MappedAligner.from_files(transition_model_path, tree, lang_graph, symbols_path,
                                  disam, acoustic_scale = 1.0)
@@ -41,9 +41,11 @@ model = FTDNN()
 model.load_state_dict(torch.load(acoustic_model_path))
 model.eval()
 
+loglikes_dict = {}
 # Extract the features, decode and write output lattices
 with SequentialMatrixReader(mfccs_rspec) as mfccs_reader, \
- 	 SequentialMatrixReader(ivectors_rspec) as ivectors_reader, open(text_path) as t:
+ 	 SequentialMatrixReader(ivectors_rspec) as ivectors_reader, open(text_path) as t, \
+     DoubleMatrixWriter(loglikes_wspec) as loglikes_writer:
     for (mkey, mfccs), (ikey, ivectors), line in zip(mfccs_reader, ivectors_reader, t):
         tkey, text = line.strip().split(None, 1)
         ivectors = np.repeat(ivectors, 10, axis=0)
@@ -52,8 +54,13 @@ with SequentialMatrixReader(mfccs_rspec) as mfccs_reader, \
         x = np.expand_dims(x, axis=0)
         feats = torch.from_numpy(x)  # Convert to PyTorch tensor
         loglikes = model(feats)                  # Compute log-likelihoods
+        loglikes_dict[mkey] = loglikes
         loglikes = Matrix(loglikes.detach().numpy()[0])      # Convert to PyKaldi matrix
+        #loglikes_writer.Write(mkey, loglikes)
         out = aligner.align(loglikes, text)
         phone_alignment = aligner.to_phone_alignment(out["alignment"], phones)
         print(mkey, phone_alignment, flush=True)
         word_alignment = aligner.to_word_alignment(out["best_path"], wb_info)
+
+with open('loglikes.pickle', 'wb') as handle:
+    pickle.dump(loglikes_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
