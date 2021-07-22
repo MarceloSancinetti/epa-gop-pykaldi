69935ce0c5ff398f1ecabdc1b93b0f15a6f68088
msancinetti@dc.uba.ar <msancinetti@dc.uba.ar>
2021-07-22 12:57:05 -0300

    Added missing path in run gop dataprep stage

--- run_gop.py
+++ run_gop.py
@@ -46,6 +46,7 @@ def run_data_prep(config_dict):
 				 "features-path": 			config_dict["features-path"],
 				 "conf-path":               config_dict["features-conf-path"],
 				 "labels-path":             config_dict["epa-ref-labels-dir-path"],
+				 "pytorch-models-path":     config_dict["pytorch-models-path"],
 				 "librispeech-models-path": config_dict["librispeech-models-path"],
 				 
 				 "libri-chain-mdl-path":    config_dict["librispeech-models-path"] + 
