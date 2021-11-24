6c1adee935f4c34e31aeb6a7bd216a0df8a58f8f
msancinetti@dc.uba.ar <msancinetti@dc.uba.ar>
2021-11-24 10:57:53 -0300

    Added swa to free state dict space

--- src/scripts/free_state_dict_space.py
+++ src/scripts/free_state_dict_space.py
@@ -6,7 +6,12 @@ if __name__ == '__main__':
 	state_dict_dir_path = sys.argv[1]
 	for state_dict_name in os.listdir(state_dict_dir_path):
 		state_dict_name_split = re.split('[. -]',state_dict_name)
-		epoch = int(state_dict_name_split[-2])
+		
+		if "swa" in state_dict_name_split[-2]:
+			epoch = int(state_dict_name_split[-2].split("_")[0])
+		else:
+			epoch = int(state_dict_name_split[-2]) 
+
 		if epoch % 25 != 0:
 			full_path = os.path.join(state_dict_dir_path, state_dict_name)
 			os.system("rm " + full_path)
